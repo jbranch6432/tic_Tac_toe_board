b065473baf3bf2f2d533b47099a7406287d05bd6
Jamie Branch <schwung988@yahoo.com>
2024-04-26 00:17:31 -0400

    add script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const playerX = "X";
+const playerO = "O";
+const winnerCombinations = [
+    [1,2,3],
+    [1,4,7],
+    [4,5,6],
+    [2,5,8],
+    [7,8,9],
+    [3,6,9],
+    [1,5,9],
+    [3,5,7],
+];
+
