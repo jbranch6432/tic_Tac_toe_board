1919e06a6bae8f2de2537a71c30160e8925aa6e7
Jamie Branch <schwung988@yahoo.com>
2024-05-21 08:28:31 -0400

    add DOM elements to JS

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const playerX = "X";
 const playerO = "O";
 const winnerCombinations = [
-    [1,2,3],
+    [0,1,2],
+    [0,3,6],
+    [3,4,5],
     [1,4,7],
-    [4,5,6],
+    [6,7,8],
     [2,5,8],
-    [7,8,9],
-    [3,6,9],
-    [1,5,9],
-    [3,5,7],
+    [0.4,8],
+    [2,4,6],
 ];
 
+const cellElements = document.querySelectorAll('[data-cell-index]');
+const boardElement = document.getElementById('[board-container]');
+const winningMessage = document.getElementById('[winningMessage]');
+const restartButton = document.getElementById('[resart-button]');
